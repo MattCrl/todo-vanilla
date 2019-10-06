ee65e3a2179891eada1938c8bf2db7642ae0f629
Matt Crl <matth.carlevaris@gmail.com>
2019-10-06 20:28:33 +0200

    Add some new features

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const todoField = document.getElementById("new-todo")
 const todoList  = document.getElementById("todo-list")
+const clearAllButton = document.getElementById("clearAllButton")
+let taskNumber = 1
 
 todoField.addEventListener("keypress", function(e) {
   console.log(e.which)
   console.log(todoField.value)
   let keyPressed = e.which
   if (keyPressed === 13) {
-    todoList.innerHTML += `<li> ${todoField.value} </li>`
+    todoList.innerHTML += `<li class="list-group-item" id="task-${taskNumber}"><input type="checkbox" id="checkbox-${taskNumber}"> ${todoField.value} <span class="pull-right remove-button" id="remove-task-${taskNumber}"><i class="fa fa-times"></i></span></li>`
+    todoField.value = ''
+    taskNumber += 1
   }
 })
+
+clearAllButton.addEventListener("click", function() {
+  console.log("test")
+  todoList.innerHTML = "";
+  taskNumber = 1
+})
+
+document.addEventListener("click", function() {
+  console.log(this)
+})
+console.log(this)
